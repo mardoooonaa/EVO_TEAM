b9301712869500a412679eea91df157a2f39096b
evoteamdev <texaposod@lillemap.net>
2017-03-19 12:49:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,3 +19,6 @@ redis-server
 echo -e "${CYAN}Installation Completed! Create a bot with creator.lua (lua creator.lua)${NC}"
 
 exit
+--install.sh with lua by EVO_TEAM
+--ch: @dev_evo
+--botdev: @EVOtmBot
