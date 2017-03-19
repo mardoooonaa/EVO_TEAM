cb7cad166cc4b395462ffe8b510ef1b3ffd0e740
evoteamdev <texaposod@lillemap.net>
2017-03-19 12:18:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-wget "https://valtman.name/files/telegram-cli-1222"
 
 sudo apt-get update
 
-redis-server
 
 cd EVO_TEAM 
 
@@ -17,12 +15,6 @@ chmod +x evo.sh
 redis-server
 
 
-
-RED='\033[0;31m'
-
-NC='\033[0m'
-
-CYAN='\033[0;36m'
 
 echo -e "${CYAN}Installation Completed! Create a bot with creator.lua (lua creator.lua)${NC}"
 
